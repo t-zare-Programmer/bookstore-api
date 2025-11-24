7edb977bd819831624f5232826a8393617383263
Tayebe Zare <t.zare1093@gmail.com>
2025-11-24 21:52:19 +0330

    Fixed change  ViewSets and Remove __pycache__ from Git tracking

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,AllowAny
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CategorySerializer, AuthorSerializer, BookSerializer
